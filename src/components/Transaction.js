e4ce100aeaf793badff96e29a8db29959c6f8e31
cdsalty <soltiscd@gmail.com>
2020-11-02 16:48:02 -0500

    finished the functionality for the History; ready to use context inside Balance component

---
+++ src/components/Transaction.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  return (
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>{transaction.text}<span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-button">x</button>
+    </li>
+  );
+}
+
+export default Transaction;
+
+
+
+
+/*
+Before for reviewing after...
+<li className="minus">{transaction.text}<span>-$400</span><button className="delete-button">x</button>
+</li>
+
+To make the number always reflect positive so it doesn't always have a minus sign, do the following:
+<span>{sign}${transaction.amount}</span>
+CHANGED TO
+<span>{sign}${Math.abs(transaction.amount)}</span>
+THIS IS BECAUSE THE TRANSACTION AMOUNT BEING ENTERED WAS CARRYING A ' - ' OR A ' + ';
+*/
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
+import Transaction from './Transaction';
+
 import { GlobalContext } from '../context/GlobalState'; // to get the value of state, pass it to useContext
 
-const TransactionList = () => {
 
+const TransactionList = () => {
   // const context = useContext(GlobalContext);
   // console.log(context); // makes up transactions...
   const { transactions } = useContext(GlobalContext);
@@ -11,10 +13,7 @@ const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (
-          <li className="minus">
-            {transaction.text} <span>-${transaction.amount}</span><button className="delete-button">x</button>
-          </li>
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </>
@@ -24,3 +23,4 @@ const TransactionList = () => {
 export default TransactionList
 
 // NEED TO ADDRESS THE -$ THAT SHOWS ON EACH TRANSACTION ON THE LIST (also tenary operator for className)
+// " <Transaction transaction={transaction} /> " is referred to as a list and therefore must have a unique key
